a60c8d4803f65267126f7b8cabfd50ccd27fa81b
joca2511 <simoesruy@gmail.com>
2024-09-10 09:23:40 -0300

    embunitado (pularEspaco agora eh utilizado novamente)

--- Lexico.java
+++ Lexico.java
@@ -6,6 +6,9 @@ public class Lexico {
     private List<Token> tokens;
     private List<AFD> afds;
     private CharacterIterator code;
+    int linha;
+    int posultimoln;
+    int coluna;
 
     public Lexico(String code){
         tokens = new ArrayList<>();
@@ -15,34 +18,29 @@ public class Lexico {
         
 
     }
-    public void pularEspaco(){
+    public void pularEspaco(){ 
         
         while (code.current() == ' ' || code.current() == '\n') {
-           
+            if (code.current() == '\n'){
+                posultimoln = code.getIndex();
+                linha+=1;
+            }
+            coluna = code.getIndex() - posultimoln;
             code.next();
         }
         
     }
     public List<Token> getTokens(){
         boolean aceito;
-        int linha = 0;
-        int posultimoln = 0;
-        int coluna = 0;
+        linha = 0;
+        posultimoln = 0;
+        coluna = 0;
         while(code.current() != CharacterIterator.DONE){ //percorre todo o arquivo, roda até EOF
             aceito = false;
-            //pularEspaco();
-            while (code.current() == ' ' || code.current() == '\n') { //roda ate chegar no 1o simbolo
-                if (code.current() == '\n'){
-                    posultimoln = code.getIndex();
-                    linha +=1;
-                }
-                coluna = code.getIndex() - posultimoln;
-                code.next();
-                
-            }
+            pularEspaco();
             
             if (code.current() == CharacterIterator.DONE){ //evita casos onde o arquivo nao contem nenhum simbolo do alfabeto
-                break;
+                break; //sai do while
             }
             for (AFD afd:afds){ //percorre todos os afd
                 int posatual = code.getIndex();
