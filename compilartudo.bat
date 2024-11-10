c0c74b151e1d4aaec78523142b6f3c14dc9831dd
joca2511 <143613939+joca2511@users.noreply.github.com>
2024-11-10 15:47:45 -0300

    Add files via upload

--- compilartudo.bat
+++ compilartudo.bat
@@ -4,12 +4,6 @@ javac -cp %cd% *.java
 
 java -cp %cd% MainAutomato %1 %2 > logs.txt
 
+if "%2" == "c" gcc traducao.c && .\a.exe
+if "%2" == "j" javac -cp %cd% traducao.java && java traducao
 
-if "%2" == "c"(
-    gcc traducao.c
-    .\a.exe
-)
-else (
-    javac -cp %cd% traducao.java
-    java -cp %cd% traducao
-)
